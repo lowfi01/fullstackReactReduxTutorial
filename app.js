c7fb758fb982c4e5a56486c1390afd903bb4b58d
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-08-23 19:23:09 +1000

    setup basic API - using callback method & mongoose

--- app.js
+++ app.js
@@ -14,13 +14,62 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// un-comment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// APIs
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bookshop', { useMongoClient: true});
+
+
+var Books = require('./models/books.js');
+
+// ----->>> POST BOOKS <<<-------
+app.post('/books', (req, res) => {
+    var book = req.body;
+
+    Books.create(book, function(err, books) {
+      if(err){
+        throw err;
+      }
+
+      res.json(books);
+    })
+});
+
+
+// ------>>> GET BOOKS <<<------------
+app.get('/books', function(req, res){
+  Books.find((err, books) => {
+    if(err){
+      throw err
+    }
+
+    res.json(books)
+  })
+})
+
+// ------>>> DELETE BOOKS <<<----------
+app.delete('/books/:_id', (req, res) => {
+  var query = {_id: req.params._id};
+
+  Books.remove(query, (err, books) => {
+    if(err){
+      throw err;
+    }
+
+    res.json(books)
+  })
+});
+
+// END APIs
+
+
 
 app.get('*', (req, res) => {
   // resolve & sendFile, public/index.html
